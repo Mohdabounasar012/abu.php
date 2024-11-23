4be480b20c9dfb925d0c09e35ad1fbc5f8aa94af
Mohd Abou Nasar <abounasar021@gmail.com>
2024-11-23 13:50:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php include('header.php'); ?>
 
 <main>
-    <h2>Welcome to My PHP Website</h2>
+    <h2>Welcome to My PHP Website 12</h2>
     <p>This is a simple website built with PHP. You can navigate to the About or Contact pages using the menu above.</p>
 </main>
 
